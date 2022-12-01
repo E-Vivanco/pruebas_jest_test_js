3ebb19e2a5bb94c7744a20574270afee023cecc9
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-01 19:33:18 +0000

    4° act_commit

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const sum = (a,b) => {
 
 // solo un registro en consola para nosotros.
 console.log(sum(7,3))
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = { sum };
